7e89b68a9d59a7a08da597ffda6eb35d80ec026f
lykasbongbongbong <lykaslin1022@gmail.com>
2021-03-22 04:11:23 +0800

    bp need refinement

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -38,12 +38,15 @@ class Net:
         self.lr = 1.2
         self.hidden_layer_neurons = [20, 7, 1]
         self.epochs = 10000
-        self.EPS = 1e-3
+        self.eps = 1e-3
         self.W = [None, np.random.randn(20, 2), np.random.randn(7, 20), np.random.randn(1, 7)]
         self.b = [None, np.random.randn(20, 1), np.random.randn(7, 1), np.random.randn(1, 1)]
         self.Z = [None, np.random.randn(20, 1), np.random.randn(7, 1), np.random.randn(1, 1)]
         self.a = [None, np.random.randn(20, 1), np.random.randn(7, 1), np.random.randn(1, 1)]
-      
+        self.gradW = [None, np.random.randn(20, 2), np.random.randn(7, 20), np.random.randn(1, 7)]
+        self.gradb = [None, np.random.randn(20, 1), np.random.randn(7, 1), np.random.randn(1, 1)]
+        self.gradZ = [None, np.random.randn(20, 1), np.random.randn(7, 1), np.random.randn(1, 1)]
+        self.grada = [None, np.random.randn(20, 1), np.random.randn(7, 1), np.random.randn(1, 1)]
     
     def sigmoid(self, x):
             #這邊不能用import math的math.exp 因為他是array math指支援size-1 arrays 
@@ -71,40 +74,57 @@ class Net:
         return float(loss)
     
     def backward(self, gt_y, pred_y):
-        
-        batch_size=gt_y.shape[1]
-        # bp
-        grad_a3=-(gt_y/(pred_y+self.EPS)-(1-gt_y)/(1-pred_y+self.EPS))
-       
-        grad_z3=grad_a3*der_sigmoid(self.a[3])
-        
-        grad_W3=grad_z3@self.a[2].T*(1/batch_size)
-        
+        #no work(need refinement)
+        # batch_size=y.shape[1]
+        # self.grada[3] = -(np.divide(y, self.a[3]+self.eps) + np.divide(1-y, 1-self.a[3]+self.eps))
+        # self.gradZ[3] = np.multiply(self.grada[3], der_sigmoid(self.a[3])) 
+        # self.gradW[3] = np.matmul(self.gradZ[3], self.a[2].T)*(1/batch_size)
+        # self.gradb[3] = np.sum(self.gradZ[3], axis=1, keepdims=True)*(1/batch_size)
 
-        grad_b3=np.sum(grad_z3,axis=1,keepdims=True)*(1/batch_size)
-        
-        grad_a2=self.W[3].T@grad_z3
-        grad_z2=grad_a2*der_sigmoid(self.a[2])
-        grad_W2=grad_z2@self.a[1].T*(1/batch_size)
-        grad_b2=np.sum(grad_z2,axis=1,keepdims=True)*(1/batch_size)
-            
-        grad_a1=self.W[2].T@grad_z2
-        grad_z1=grad_a1*der_sigmoid(self.a[1])
-        grad_W1=grad_z1@self.a[0].T*(1/batch_size)
-        grad_b1=np.sum(grad_z1,axis=1,keepdims=True)*(1/batch_size)
-        
-        # update
-       
-        self.W[1]-=self.lr*grad_W1
-        self.W[2]-=self.lr*grad_W2
-        self.W[3]-=self.lr*grad_W3
-        self.b[1]-=self.lr*grad_b1
-        self.b[2]-=self.lr*grad_b2
-        self.b[3]-=self.lr*grad_b3
-        
-        return
+        # self.grada[2] = -(np.divide(y, self.a[2]+self.eps) + np.divide(1-y, 1-self.a[2]+self.eps))
+        # self.gradZ[2] = np.multiply(self.grada[2], der_sigmoid(self.a[2])) 
+        # self.gradW[2] = np.matmul(self.gradZ[2], self.a[1].T)*(1/batch_size)
+        # self.gradb[2] = np.sum(self.gradZ[2], axis=1, keepdims=True)*(1/batch_size)
 
+        # self.grada[1] = -(np.divide(y, self.a[1]+self.eps) + np.divide(1-y, 1-self.a[1]+self.eps))
+        # self.gradZ[1] = np.multiply(self.grada[1], der_sigmoid(self.a[1])) 
+        # self.gradW[1] = np.matmul(self.gradZ[1], self.a[0].T)*(1/batch_size)
+        # self.gradb[1] = np.sum(self.gradZ[1], axis=1, keepdims=True)*(1/batch_size)
+
+        #work(for reference)
+        # batch_size=gt_y.shape[1]
+        # grad_a3=-(gt_y/(pred_y+self.eps)-(1-gt_y)/(1-pred_y+self.eps))
+        # grad_z3=grad_a3*der_sigmoid(self.a[3])
+        # grad_W3=grad_z3@self.a[2].T*(1/batch_size)
+        # grad_b3=np.sum(grad_z3,axis=1,keepdims=True)*(1/batch_size)
     
+        # grad_a2=self.W[3].T@grad_z3
+        # grad_z2=grad_a2*der_sigmoid(self.a[2])
+        # grad_W2=grad_z2@self.a[1].T*(1/batch_size)
+        # grad_b2=np.sum(grad_z2,axis=1,keepdims=True)*(1/batch_size)
+            
+        # grad_a1=self.W[2].T@grad_z2
+        # grad_z1=grad_a1*der_sigmoid(self.a[1])
+        # grad_W1=grad_z1@self.a[0].T*(1/batch_size)
+        # grad_b1=np.sum(grad_z1,axis=1,keepdims=True)*(1/batch_size)
+
+        # # update
+        # self.W[1]-=self.lr*grad_W1
+        # self.W[2]-=self.lr*grad_W2
+        # self.W[3]-=self.lr*grad_W3
+        # self.b[1]-=self.lr*grad_b1
+        # self.b[2]-=self.lr*grad_b2
+        # self.b[3]-=self.lr*grad_b3
+        
+        # return
+    def update_weight(self):
+        
+        for i in range(1,4):
+            # print(i)
+            # print(self.gradW[i].shape)
+            self.W[i] -= self.lr * self.gradW[i]
+            self.b[i] -= self.lr * self.gradb[i]
+        return
         
 
 def generate_linear(n=100):
