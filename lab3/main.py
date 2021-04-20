a5c7273eed7a9e24153854177cdd091acd5fe27b
lykasbongbongbong <lykaslin1022@gmail.com>
2021-04-20 23:35:13 +0800

    train / main.py update

--- lab3/main.py
+++ lab3/main.py
@@ -1,10 +1,29 @@
 import dataloader
 import train
+from torch.optim import Adam
+import torch.nn as nn
+import torch
+
 def main():
+    batch_size = 64
+    learning_rate = 1e-2
+    epochs = 300
+
+    
     #處理data
     train_data, train_label, test_data, test_label = dataloader.read_bci_data()
     #train
     model = train.EEGNet()
+    optimizer = Adam(model.parameters(), lr=learning_rate)
+    loss = nn.CrossEntropyLoss()
+
+
+    #檢查有沒有GPU:
+    if torch.cuda.is_available():
+        print("here!")
+        model = model.cuda()
+        loss = loss.cuda()
+    
     #test
     print(model)
 
--- lab3/train.py
+++ lab3/train.py
@@ -7,12 +7,32 @@ class EEGNet(nn.Module):
         super(EEGNet,self).__init__()
         self.firstconv=nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=(1,51), stride=(1,1), padding=(0,25), bias=False),
-            nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+            nn.BatchNorm2d(16, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
         )
         self.depthwiseConv = nn.Sequential(
             nn.Conv2d(16,32,kernel_size=(2,1), stride=(1,1), groups=16, bias=False),
             nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-            nn.ELU(alpha=1.0)
+            nn.ELU(alpha=1.0),
+            nn.AvgPool2d(kernel_size=(1,4), stride=(1,4), padding=0),
+            nn.Dropout(p=0.25)
         )
-        
-   
+        self.separableConv = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=(1, 15), stride=(1,1), padding=(0, 7), bias=False),
+            nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            nn.ELU(alpha=1.0),
+            nn.AvgPool2d(kernel_size=(1,8), stride=(1,8), padding=0),
+            nn.Dropout(p=0.25)
+        )
+        self.classify = nn.Sequential(
+            nn.Linear(in_features=736, out_features=2, bias=True)
+        )
+    
+    def forward(self, x):
+        out = self.firstconv(x)
+        out = self.depthwiseConv(out)
+        out = self.separableConv(out)
+        out = out.view(out[0], -1)
+        out = self.classify(out)
+        return out
+    
+    
