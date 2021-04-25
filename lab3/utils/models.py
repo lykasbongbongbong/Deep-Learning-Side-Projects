cc03ebcfac9ae8a3374a0670fa36ea500de4c0ab
lykasbongbongbong <lykaslin1022@gmail.com>
2021-04-25 18:00:42 +0800

    only output the highest acc score

--- lab3/utils/models.py
+++ lab3/utils/models.py
@@ -1,6 +1,43 @@
 import torch 
 import torch.nn as nn
 
+class EEGNet(nn.Module):
+    def __init__(self, activation=nn.ReLU()):
+        super(EEGNet,self).__init__()
+        self.activation = activation
+        self.firstconv=nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=(1,51), stride=(1,1), padding=(0,25), bias=False),
+           nn.BatchNorm2d(16, eps=1e-5, momentum=0.1, affine=True, track_running_stats=True)
+        )
+        self.depthwiseConv = nn.Sequential(
+            nn.Conv2d(16,32,kernel_size=(2,1), stride=(1,1), groups=16, bias=False),
+            nn.BatchNorm2d(32, eps=1e-5, momentum=0.1, affine=True, track_running_stats=True),
+            activation, 
+            nn.AvgPool2d(kernel_size=(1,4), stride=(1,4), padding=0),
+            nn.Dropout(p=0.25)
+        )
+        self.separableConv = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=(1, 15), stride=(1,1), padding=(0, 7), bias=False),
+            nn.BatchNorm2d(32, eps=1e-5, momentum=0.1, affine=True, track_running_stats=True),
+            activation, 
+            nn.AvgPool2d(kernel_size=(1,8), stride=(1,8), padding=0),
+            nn.Dropout(p=0.25)
+        )
+        self.classify = nn.Sequential(
+            nn.Linear(in_features=736, out_features=2, bias=True)
+        )
+    
+    def forward(self, x):
+        out = self.firstconv(x)
+        # print("[Batch_size, feature_map_size, feature_map_width, feature_map_height]")
+        # print(out.shape)
+        out = self.depthwiseConv(out)
+        out = self.separableConv(out)
+        out = out.view(out.shape[0], -1)
+        out = self.classify(out)
+        return out
+
+
 class DeepConvNet(nn.Module):
     def __init__(self, activation=nn.ReLU()):
         super(DeepConvNet,self).__init__()
@@ -52,36 +89,3 @@ class DeepConvNet(nn.Module):
         out = self.classify(out)
         return out
 
-class EEGNet(nn.Module):
-    def __init__(self, activation=nn.ReLU()):
-        super(EEGNet,self).__init__()
-        self.activation = activation
-        self.firstconv=nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=(1,51), stride=(1,1), padding=(0,25), bias=False),
-            nn.BatchNorm2d(16, eps=1e-5, momentum=0.1, affine=True, track_running_stats=True)
-        )
-        self.depthwiseConv = nn.Sequential(
-            nn.Conv2d(16,32,kernel_size=(2,1), stride=(1,1), groups=16, bias=False),
-            nn.BatchNorm2d(32, eps=1e-5, momentum=0.1, affine=True, track_running_stats=True),
-            activation, 
-            nn.AvgPool2d(kernel_size=(1,4), stride=(1,4), padding=0),
-            nn.Dropout(p=0.25)
-        )
-        self.separableConv = nn.Sequential(
-            nn.Conv2d(32, 32, kernel_size=(1, 15), stride=(1,1), padding=(0, 7), bias=False),
-            nn.BatchNorm2d(32, eps=1e-5, momentum=0.1, affine=True, track_running_stats=True),
-            activation, 
-            nn.AvgPool2d(kernel_size=(1,8), stride=(1,8), padding=0),
-            nn.Dropout(p=0.25)
-        )
-        self.classify = nn.Sequential(
-            nn.Linear(in_features=736, out_features=2, bias=True)
-        )
-    
-    def forward(self, x):
-        out = self.firstconv(x)
-        out = self.depthwiseConv(out)
-        out = self.separableConv(out)
-        out = out.view(out.shape[0], -1)
-        out = self.classify(out)
-        return out
